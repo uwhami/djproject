545200299e9093edeacd33171af8d51ff6a502a2
uwhami <uwhami@DJui-MacBookPro.local>
2021-01-05 11:36:03 +0900

    https://github.com/uwhami/djproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WEB</h1>
+      World wide web
+      <ul>
+        <li>
+          <a href="1.html">HTML</a>
+        </li>
+      </ul>
     </div>
   );
 }
